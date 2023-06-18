6656bc41609a5a9fd3df16c8ab242a2b86e01194
Alok raj <alokraj2287@gmail.com>
2023-06-19 01:50:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/findingindexofsumelementinanarray/Finding Index Value.kt
@@ -0,0 +1,20 @@
+package com.example.findingindexofsumelementinanarray
+
+
+fun main() {
+    val arr = intArrayOf(2, 3, 4, 6, 7, 9)
+    val target = 9
+    findSumOfIndexValue(arr, target)?.let { (i1, i2) ->
+        println("${arr[i1]},${arr[i2]}")
+    }
+}  
+
+fun findSumOfIndexValue(arr: IntArray, target: Int): Pair<Int, Int>? {
+    val map = mutableMapOf<Int, Int>()
+    for (i in arr.indices) {
+        val targetValue = target - arr[i]
+        map[targetValue]?.let { return it to i }
+        map[arr[i]]= i
+    }
+    return null
+}
